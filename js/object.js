de21bdb7679edff10ac1559a62859137eaf52c62
itahm <tomatoalive@gmail.com>
2016-03-31 19:09:35 +0900

    2016-03-31 ,office

--- js/object.js
+++ js/object.js
@@ -21,7 +21,11 @@ var ITAhM = ITAhM || {};
 	
 	ITAhM.Path = function (container) {
 		this.initialize(container);
-	}
+	};
+	
+	ITAhM.Calendar = function (calendar, handler) {
+		this.initialize(calendar, handler);
+	};
 	
 	ITAhM.QueryParser.prototype = {
 		map: {},
@@ -193,4 +197,137 @@ var ITAhM = ITAhM || {};
 			this.distance[this.distance.length] = "Z"
 		}
 	};
+	
+	ITAhM.Calendar.prototype = {
+		header: document.createElement("div"),
+		body: document.createElement("div"),
+		date: document.createElement("div"),
+		button: document.createElement("div"),
+		year: document.createElement("span"),
+		month: document.createElement("select"),
+		btnPrev: document.createElement("button"),
+		btnCur: document.createElement("button"),
+		btnNext: document.createElement("button"),
+		initialize: function (id, handler) {
+			this.calendar = document.getElementById(id);
+			this.callback = handler;
+			
+			//event
+			this.month.onchange = function () {
+				this.move(new Date(this.year.textContent, this.month.value));
+			}.bind(this);
+			
+			this.btnPrev.onclick = function () {
+				this.move(new Date(this.year.textContent, Number(this.month.value) -1));
+			}.bind(this);
+			
+			this.btnCur.onclick = function () {
+				var date = new Date();
+				
+				date.setHours(0, 0, 0, 0);
+				
+				this.callback(date);
+			}.bind(this);
+			
+			this.btnNext.onclick = function () {
+				this.move(new Date(this.year.textContent, Number(this.month.value) +1));
+			}.bind(this);
+			
+			// header
+			this.header.appendChild(this.date);
+			this.header.appendChild(this.button);
+			
+			this.date.appendChild(this.year);
+			this.date.appendChild(this.month);
+			
+			for (var i=0, opt; i<12;) {
+				opt = document.createElement("option");
+				
+				opt.value = i++;
+				opt.text = i<10? "0" + i: i;
+				
+				this.month.appendChild(opt);
+			}
+			
+			this.button.appendChild(this.btnPrev).textContent = "<";
+			this.button.appendChild(this.btnCur).textContent = ".";
+			this.button.appendChild(this.btnNext).textContent = ">";
+			
+			this.calendar.appendChild(this.header);
+			
+			// body
+			var ul = document.createElement("ul"),
+				dayArray = ["일", "월", "화", "수", "목", "금", "토"];
+			
+			for (var j=0; j<7; j++) {
+				ul.appendChild(document.createElement("li")).textContent = dayArray[j];
+			}
+			
+			this.body.appendChild(ul);
+			
+			for (var i=0; i<6; i++) {
+				ul = document.createElement("ul");
+				
+				for (var j=0; j<7; j++) {
+					ul.appendChild(document.createElement("li")).textContent = 1;
+				}
+				
+				this.body.appendChild(ul);
+			}
+			
+			this.calendar.appendChild(this.body);
+			
+			this.move(new Date());
+		},
+		
+		move: function (date) {
+			this.year.textContent = date.getFullYear();
+			this.month.value = date.getMonth();
+			
+			date.setHours(0, 0, 0, 0);
+			
+			this.set(date);
+		},
+		
+		set: function (date) {
+			var year = date.getFullYear(),
+				month = date.getMonth(),
+				dateArray = Array(6*7).fill(0),
+				index, lastDate;
+			
+			date.setDate(1);
+			index = date.getDay();
+			
+			date.setMonth(month +1, 0);
+			lastDate = date.getDate();
+			
+			for (var i=1; i <= lastDate; i++, index++) {
+				dateArray[index] = i;
+			}
+			
+			for (var i=0, _i=dateArray.length, index=0, col, row = this.body.childNodes[1]; i<_i; i++) {
+				col = row.childNodes[index];
+				
+				if (dateArray[i]) {
+					col.textContent = dateArray[i];
+					col.className = "valid";
+					col.onclick = function (y, m, d) {
+						this.callback(new Date(y, m, d, 0, 0, 0, 0));
+					}.bind(this, year, month, dateArray[i]);
+				}
+				else {
+					col.textContent = "";
+					col.className = "";
+					col.onclick = undefined;
+				}
+				
+				if (++index > 6) {
+					index = 0;
+					
+					row = row.nextSibling;
+				}
+			}
+		}
+	};
+
 }) (window);
