8663ec047ef36d7805e21355b71db8f7ede460a2
ITAhM <ITAhM@ITAhM>
2017-03-16 14:18:54 +0900

    fix

--- js/ITAhM.js
+++ js/ITAhM.js
@@ -17,10 +17,10 @@ Array.prototype.fill = Array.prototype.fill || function (val) {
 	
 	ITAhM.util = {
 		enterpriseFromOID: function (oid) {
-			var match = /^1\.3\.6\.1\.4\.1\.(\d+)\./.exec(oid);
+			var match = /^1\.3\.6\.1\.4\.1\.(\d+)/.exec(oid);
 			
 			if (match) {
-				return ITAhM.snmp.enterprise[match[1]];
+				return ITAhM.snmp.enterprise[match[1]] || match[1];
 			}
 		},
 		
